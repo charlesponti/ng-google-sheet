e8cbd5fe440575f1f7d2bd12d6483e050063a9f9
CJ Ponti <cjponti@gmail.com>
2015-01-03 18:19:51 +0000

    Update HomeCtrl

--- src/scripts/controllers/home.js
+++ src/scripts/controllers/home.js
@@ -3,8 +3,42 @@
 /**
  * @ngInject
  */
-module.exports = function($scope, GoogleSheets) {
+module.exports = function() {
 
-  $scope.title = "Welcome to Facade-Angular";
+  this.config = {
+    sheet: '1jEAO4g_C0NfGkMrLiqIIcXxbOmbfY5mvZ7GzevSi_5c',
+    columns: [
+      {
+        field: 'ticker',
+        header: 'Ticker'
+      },
+      {
+        field: 'industry',
+        header: 'Industry'
+      },
+      {
+        field: 'marketcap',
+        header: 'Market Cap',
+        type: 'money'
+      },
+      {
+        field: 'change',
+        header: 'Change',
+        type: 'percentage'
+      },
+      {
+        field: 'price',
+        header: 'Price',
+        type: 'money'
+      },
+      {
+        field: 'volume',
+        header: 'Volume',
+        type: 'number'
+      }
+    ]
+  };
+
+  this.title = "Welcome to Facade-Angular";
 
 };
