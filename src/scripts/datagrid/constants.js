0bc41c6c96204dd79e8ebceb603ed94e5ad88e3e
CJ Ponti <cjponti@gmail.com>
2015-01-03 12:46:37 +0000

    Move module constants

--- src/scripts/datagrid/constants.js
+++ src/scripts/datagrid/constants.js
@@ -1,5 +1,9 @@
 'use strict';
 
 module.exports = {
+
+  googleSheetsUrl: 'https://spreadsheets.google.com/feeds/list/:key/ow1vgqx/public/basic?alt=json',
+
   CURRENCY_SYMBOLS: ['ƒ', '$', '£', '$', '¤', '¥', '៛', '₩', '₱', '฿', '₫']
+
 };
